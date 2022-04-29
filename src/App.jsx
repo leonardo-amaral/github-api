efc6639ab3b795cfdd1ac450f21867f493dda993
Leonardo Amaral <leozero103@gmail.com>
2022-04-29 16:05:59 -0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -47,22 +47,24 @@ function App() {
     <>
       <div className="App" data-aos="fade-up">
         <Nav />
-        <div className="repo">
-          {
-            repositories.map((repositorie) => {
+        <div className="all">
+          <div className="repo">
+            {
+              repositories.map((repositorie) => {
 
-              return (
-                <div className='box' key={repositorie.name}>
-                  <div className='box-repos'>
-                    <h1 className='title'>{repositorie.name}</h1>
-                    <h2>{repositorie.language}</h2>
-                    <h3>{repositorie.description}</h3>
-                    <button onClick={() => fetchFunction(repositorie.languages_url)}>aaaaaaaa</button>
+                return (
+                  <div className='box' key={repositorie.name}>
+                    <div className='box-repos'>
+                      <h1 className='title'>{repositorie.name}</h1>
+                      <h2>{repositorie.language}</h2>
+                      <h3>{repositorie.description}</h3>
+                      <button onClick={() => fetchFunction(repositorie.languages_url)}>aaaaaaaa</button>
+                    </div>
                   </div>
-                </div>
-              )
-            })
-          }
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     </>
